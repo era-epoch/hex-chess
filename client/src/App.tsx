222b41416ffc185d4c6a8b3884f4eb1c7f8ccd6d
jacob-sahl <jacobsahlmueller@gmail.com>
2023-07-28 23:32:57 -0400

    env stuff

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from 'react';
+import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import AlertBox from './Components/AlertBox';
 import CreateGameDialogue from './Components/CreateGameDialogue';
@@ -11,22 +11,29 @@ import { resetBoard } from './State/Slices/gameSlice';
 import './Styles/alert.css';
 import './Styles/dialogue.css';
 import './Styles/uibutton.css';
+import { IS_PROD } from './env';
 import { wsConnect, wsDisconnect } from './websocketMiddleware';
 
-let socketURL = `${window.location.hostname}:5000`;
+let socketURL: string;
+if (IS_PROD) {
+  socketURL = `${window.location.hostname}:5000`;
+} else {
+  socketURL = `https://www.hex-chess.io:5000`;
+}
 
 function App() {
   const dispatch = useDispatch();
 
-  const firstLoad = useRef(true);
-  if (firstLoad.current) {
-    // This runs twice but I think only in dev mode(???)
-    dispatch(resetBoard());
-    firstLoad.current = false;
-  }
+  // const firstLoad = useRef(true);
+  // if (firstLoad.current) {
+  //   // This runs twice but I think only in dev mode(???)
+  //   dispatch(resetBoard());
+  //   firstLoad.current = false;
+  // }
 
   useEffect(() => {
     console.log(socketURL);
+    dispatch(resetBoard());
     dispatch(wsConnect(socketURL));
     return () => {
       dispatch(wsDisconnect(socketURL));
---
+++ client/src/env.ts
@@ -0,0 +1,3 @@
+export const IS_PROD = process.env.NODE_ENV === 'production';
+
+console.log(process.env);
